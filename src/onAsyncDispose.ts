cacd9bbd8ae15ec4e19c968a164f1c94c696ea6e
Frédérique Mittelstaedt <git@gfm.io>
2024-01-18 11:05:20 +0000

    add triple slash directive

--- src/onAsyncDispose.ts
+++ src/onAsyncDispose.ts
@@ -1,3 +1,5 @@
+/// <reference lib="esnext.disposable" />
+
 export const onAsyncDispose = (action: () => void): AsyncDisposable => ({
   [Symbol.asyncDispose]: async () => {
     await Promise.resolve(action());
--- src/onDispose.ts
+++ src/onDispose.ts
@@ -1,3 +1,5 @@
+/// <reference lib="esnext.disposable" />
+
 export const onDispose = (action: () => void): Disposable => ({
   [Symbol.dispose]: () => {
     action();
